8ceb28b2422d1ec800bbb79af245fdc19175cb69
Jesse <nguyenjessev@gmail.com>
2020-08-19 20:46:10 -0400

    Add LinkedList class skeleton

--- linked_list.rb
+++ linked_list.rb
@@ -1,7 +1,29 @@
 # frozen-string-literal: true
 
 module LinkedList
+  # This class represents the linked list
   class LinkedList
+    attr_accessor :head, :size
+
+    def initialize
+      self.size = 0
+      self.head = nil
+      self.tail = nil
+    end
+
+    def append(value); end
+
+    def prepend(value); end
+
+    def at(index); end
+
+    def pop; end
+
+    def contains?(value); end
+
+    def find(value); end
+
+    def to_s; end
   end
 
   # This class represents a single node in a linked list
